18612028ac3dce7a97824c15931ab63620b38ac7
Limpin0123 <120259965+Limpin0123@users.noreply.github.com>
2025-07-23 14:50:16 +0200

    minor build error fix

--- Runtime/Console/Custom Console Command Caller.cs
+++ Runtime/Console/Custom Console Command Caller.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using CustomConsole.Runtime.Logger;
-using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
 using UnityEngine.UI;
 
